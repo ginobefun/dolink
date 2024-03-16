24d649549f686333b071e6ec79ae5ae2cdcb6420
Gino Zhang <ginobefun@gmail.com>
2024-03-16 10:03:25 +0800

    add vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react"
 import "./globals.css";
 import Header from "@/components/header";
 import Footer from "@/components/footer";
@@ -22,6 +23,7 @@ export default function RootLayout({
         <Header />
         {children}
         <Footer />
+        <Analytics />
       </body>
     </html>
   );
